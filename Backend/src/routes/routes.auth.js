3f6a3846bfa7a63607b15935130f6138bf03ad08
Udara Nalawansa <udarakavishka13@gmail.com>
2024-05-01 00:02:23 +0530

    feat: User Validation added

--- Backend/src/routes/routes.auth.js
+++ Backend/src/routes/routes.auth.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { register, login } = require('../controllers/controller.auth.js');
+const {  regsiterValidation, loginValidation} = require('../middleware/middleware.authvalidation.js');
 
 const router= express.Router();
 
-router.post('/register',register);
-router.post('/login',login);
+router.post('/register',regsiterValidation,register);
+router.post('/login',loginValidation, login);
 
 module.exports= router;
