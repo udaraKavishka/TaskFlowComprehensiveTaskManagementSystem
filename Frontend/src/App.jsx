b3c1c66821ce3f9a354a41aec7c87a0e2079f2b1
Udara Nalawansa <udarakavishka13@gmail.com>
2024-05-08 19:32:18 +0530

    feat: build login page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import '@fontsource/roboto/300.css'
 import '@fontsource/roboto/400.css'
-import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/500.css' 
 import '@fontsource/roboto/700.css'
 import CssBaseLine from '@mui/material/CssBaseline'
 import { ThemeProvider, createTheme } from '@mui/material/styles'
