192286ea03ac50d89d13d564d3086989eab60099
Udara Nalawansa <udarakavishka13@gmail.com>
2024-05-12 00:30:29 +0530

    feat: add routes for board controllers

---
+++ Backend/src/routes/board.routes.js
@@ -0,0 +1,31 @@
+const express= require('express')
+const router= express.Router();
+const validation = require('../middleware/validation.js')
+const tokenHandle = require('../middleware/auth.middleware.js')
+const boardController = require('../controllers/board.controller.js')
+
+
+router.post('/',tokenHandle,boardController.createBoard)
+
+router.get('/',tokenHandle,boardController.getAll)
+
+router.put('/',tokenHandle,boardController.updatePosition)
+
+router.get('/favourites',tokenHandle,boardController.getFavourites)
+
+router.put('/favourites',tokenHandle,boardController.updateFavouritePosition)
+
+router.get('/:boardId',
+// param('boardId').custom(value => {
+//     if (!validation.isObjectId(value)) {
+//       return Promise.reject('invalid id')
+//     } else return Promise.resolve()
+//   }),
+validation.validate,tokenHandle,boardController.getAll)
+
+router.put('/:boardId',validation.validate,tokenHandle,boardController.updateBoard)
+
+router.delete('/:boardId',validation.validate,tokenHandle,boardController.deleteBoard)
+
+module.exports= router
+
