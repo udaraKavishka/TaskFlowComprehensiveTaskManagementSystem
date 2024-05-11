9b9c8adf3861060c9a608469242e25790e4d50e9
Udara Nalawansa <udarakavishka13@gmail.com>
2024-05-12 00:25:09 +0530

    feat: add get one board function

--- Backend/src/controllers/board.controller.js
+++ Backend/src/controllers/board.controller.js
@@ -49,3 +49,26 @@ const updatePosition= async(req, res)=>{
         })
     }
 }
+
+const getOne= async(req,res)=>{
+    const {boardId}= req.params
+    try {
+        const board= await Board.findOne({user: req.user._id, _id:boardId})
+        if(!board){
+            return res.status(404).json('Board not found')
+        }else{
+            for(const section of sections){
+                const tasks= await Task.find({section:section.id}).populate('section').sort('-position')
+                section._doc.tasks = tasks
+            }
+            board._doc.sections = sections
+            res.status(200).json(board)
+        }
+        
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message,
+            success: false
+        })
+    }
+}
